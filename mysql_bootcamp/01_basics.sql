c2cbf4081762ca2b15d859dd7ca1449d60ec8f5b
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-01-30 12:57:11 +0100

    Update 01_basics.sql

--- mysql_bootcamp/01_basics.sql
+++ mysql_bootcamp/01_basics.sql
@@ -1,6 +1,20 @@
--- CREATE TABLE Tweets
--- (
+# First steps
+-- SHOW DATABASES;
+-- CREATE DATABASE training;
+-- USE training;
+-- DROP DATABASE training
+
+# Create table
+-- CREATE TABLE Tweets (
 -- Username VARCHAR(15),
 -- Content VARCHAR(140),
 -- Favorites INT
 --  );
+
+# How do we know it worked?
+-- SHOW TABLES;
+-- SHOW COLUMNS FROM tweets;
+-- DESC tweets; 
+
+# Deleting Tables
+-- DROP TABLE cats;
