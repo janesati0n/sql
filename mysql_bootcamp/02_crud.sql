edd0fcf908fa03f9983a965b11f58d766e2882ed
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-01-31 15:51:35 +0100

    Update 02_crud.sql

--- mysql_bootcamp/02_crud.sql
+++ mysql_bootcamp/02_crud.sql
@@ -1,21 +1,26 @@
 # Preparation
 -- DROP TABLE cats;
+-- CREATE TABLE cats (
+-- 	cat_id INT AUTO_INCREMENT,
+--     name VARCHAR(100),
+--     breed VARCHAR(100),
+--     age INT,
+--     PRIMARY KEY (cat_id)
+-- ); 
+-- INSERT INTO cats(name, breed, age) 
+-- VALUES ('Ringo', 'Tabby', 4),
+-- 	('Cindy', 'Maine Coon', 10),
+-- 	('Dumbledore', 'Maine Coon', 11),
+--     ('Egg', 'Persian', 4),
+--     ('Misty', 'Tabby', 13),
+--     ('George Michael', 'Ragdoll', 9),
+--     ('Jackson', 'Sphynx', 7);
 
---     CREATE TABLE cats (
---         cat_id INT AUTO_INCREMENT,
---         name VARCHAR(100),
---         breed VARCHAR(100),
---         age INT,
---         PRIMARY KEY (cat_id)
---     ); 
---     
---     INSERT INTO cats(name, breed, age) 
---     VALUES ('Ringo', 'Tabby', 4),
---            ('Cindy', 'Maine Coon', 10),
---            ('Dumbledore', 'Maine Coon', 11),
---            ('Egg', 'Persian', 4),
---            ('Misty', 'Tabby', 13),
---            ('George Michael', 'Ragdoll', 9),
---            ('Jackson', 'Sphynx', 7);
+# READ
+-- SELECT * FROM cats WHERE age=4;
+-- SELECT cat_id FROM cats;
+-- SELECT name, breed FROM cats;
+-- SELECT name, age FROM cats WHERE breed='Tabby';
+-- SELECT cat_id, age FROM cats WHERE cat_id=age;
 
-# Read
+# Aliases
