be4988b8c79da5358ffc5d2d3fa38495cb339bc6
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-01-31 11:36:02 +0100

    Update 01_excercises.sql

--- mysql_bootcamp/01_excercises.sql
+++ mysql_bootcamp/01_excercises.sql
@@ -14,3 +14,13 @@
 --  ('Calvin', 'Fischoeder', 70);
 
 -- select * from people;
+
+# Employees table
+-- CREATE TABLE employees (
+-- 	employee_id INT AUTO_INCREMENT PRIMARY KEY,
+-- 	last_name VARCHAR(100) NOT NULL,
+-- 	first_name VARCHAR(100) NOT NULL,
+-- 	middle_name VARCHAR(100),
+-- 	age INT NOT NULL,
+-- 	current_status VARCHAR(100) NOT NULL DEFAULT 'employed'
+-- );
