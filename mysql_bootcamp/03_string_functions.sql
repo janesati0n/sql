7f6125da87ea6ebe4f22239d8b26c43cf33a2079
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-02 11:01:28 +0100

    Update 03_string_functions.sql

--- mysql_bootcamp/03_string_functions.sql
+++ mysql_bootcamp/03_string_functions.sql
@@ -1,3 +1,7 @@
 -- CREATE DATABASE book_shop;
 -- USE book_shop;
 -- source 03_book_data.sql
+
+# CONCAT
+-- SELECT CONCAT (author_fname, ' ', author_lname) AS author_name FROM books;
+-- SELECT CONCAT_WS ('-', title, author_fname, author_lname) FROM books;
