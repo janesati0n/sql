443e836c3de0eb53ece5565327e1decf3c19efb3
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-02 12:21:34 +0100

    Update 03_string_functions.sql

--- mysql_bootcamp/03_string_functions.sql
+++ mysql_bootcamp/03_string_functions.sql
@@ -15,3 +15,19 @@
 -- SELECT SUBSTRING(title, 1, 15) AS 'short title' FROM books;
 -- SELECT SUBSTR(author_lname, 1, 1) AS initial, author_lname FROM books;
 
+# Combining String Functions
+-- SELECT CONCAT
+-- 	(
+-- 		SUBSTR(title, 1, 10),
+-- 		'...'
+-- 	) AS 'short title'
+-- FROM books;
+
+-- SELECT CONCAT
+-- 	(
+-- 		SUBSTR(author_fname, 1, 1),
+-- 		'.',
+-- 		SUBSTR(author_lname, 1, 1),
+-- 		'.'
+-- 	) AS 'author_initials'
+-- FROM books;
