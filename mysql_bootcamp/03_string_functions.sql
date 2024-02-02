26a4140792ea8d224d43d5e1a7ea9d82257d54ab
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-02 12:08:20 +0100

    Update 03_string_functions.sql

--- mysql_bootcamp/03_string_functions.sql
+++ mysql_bootcamp/03_string_functions.sql
@@ -5,3 +5,13 @@
 # CONCAT
 -- SELECT CONCAT (author_fname, ' ', author_lname) AS author_name FROM books;
 -- SELECT CONCAT_WS ('-', title, author_fname, author_lname) FROM books;
+
+# SUBSTRING
+-- SELECT SUBSTRING('Hello World', 1, 5);
+-- SELECT SUBSTRING('Hello World', 7);
+-- SELECT SUBSTRING('Hello World', -3);
+-- SELECT SUBSTRING('Hello World', -3, 1);
+
+-- SELECT SUBSTRING(title, 1, 15) AS 'short title' FROM books;
+-- SELECT SUBSTR(author_lname, 1, 1) AS initial, author_lname FROM books;
+
