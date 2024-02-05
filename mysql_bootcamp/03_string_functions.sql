89df0d28f77200695164dd44b65820366ebb5c5e
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-05 13:00:55 +0100

    Update 03_string_functions.sql

--- mysql_bootcamp/03_string_functions.sql
+++ mysql_bootcamp/03_string_functions.sql
@@ -38,3 +38,7 @@
 -- SELECT REPLACE(title, ' ', '-') FROM books;
 
 # REVERSE
+-- SELECT REVERSE('Hello World!');
+-- SELECT REVERSE(NULL);
+-- SELECT REVERSE(author_fname) FROM books;
+-- SELECT CONCAT(author_fname, REVERSE(author_fname)) FROM books;
