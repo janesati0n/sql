8df42b0ff0f225d9402d47bcde9815004bdc7ad8
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-05 13:56:48 +0100

    Update 03_string_functions.sql

--- mysql_bootcamp/03_string_functions.sql
+++ mysql_bootcamp/03_string_functions.sql
@@ -49,3 +49,37 @@
 -- SELECT CHAR_LENGTH(title) AS length, title FROM books;
 -- SELECT CONCAT(author_lname, ' is ', CHAR_LENGTH(author_lname), ' characters long') FROM books;
 
+# UPPER and LOWER
+-- SELECT UPPER('Hello');
+-- SELECT UCASE('Hello');
+-- SELECT LOWER('Hello');
+-- SELECT LCASE('Hello');
+-- SELECT CONCAT('I LOVE ', UPPER(title), ' !!!') FROM books;
+
+# Other String Functions
+-- SELECT INSERT('Hello Bobby', 6, 0, ' There');
+
+-- SELECT LEFT('omghahalol!', 3);
+-- SELECT RIGHT('omghahalol!', 4);
+-- SELECT LEFT(author_lname, 1) AS first_initial FROM books;
+
+-- SELECT REPEAT('ha', 4);
+
+-- SELECT TRIM('    san antonio   ');
+-- SELECT TRIM(LEADING '.' FROM '.....san antonio...');
+-- SELECT TRIM(TRAILING '.' FROM '.....san antonio...');
+-- SELECT TRIM(BOTH '.' FROM '.....san antonio...');
+
+# Excercise
+-- SELECT REVERSE(UPPER('Why does my cat look at me with such hatred?'));
+-- SELECT REPLACE(title, ' ', '->') from books;
+-- SELECT author_lname AS forwards, REVERSE(author_lname) AS backwards FROM books;
+-- SELECT UPPER(CONCAT(author_fname, ' ', author_lname)) AS 'full name in caps' FROM books;
+-- SELECT CONCAT(title, ' was released in ', released_year) AS blurb FROM books;
+-- SELECT title, CHAR_LENGTH(title) AS 'character count' FROM books;
+
+-- SELECT 
+-- 	CONCAT(SUBSTRING(title, 1, 10), '...') AS 'short title', 
+-- 	CONCAT(author_lname, ',', author_fname) AS author, 
+-- 	CONCAT(stock_quantity, ' in stock') AS quantity 
+-- FROM books;
