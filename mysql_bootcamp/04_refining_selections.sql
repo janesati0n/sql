ba390cf228fc4a46ff494151df6422a25598f05d
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-06 11:36:18 +0100

    Update 04_refining_selections.sql

--- mysql_bootcamp/04_refining_selections.sql
+++ mysql_bootcamp/04_refining_selections.sql
@@ -10,3 +10,12 @@
 -- SELECT DISTINCT CONCAT(author_fname, ' ', author_lname) FROM books;
 -- SELECT DISTINCT author_fname, author_lname FROM books;
 
+# ORDER BY
+-- SELECT * FROM books ORDER BY author_lname;
+-- SELECT * FROM books ORDER BY author_lname DESC;
+-- SELECT * FROM books ORDER BY released_year;
+
+-- SELECT title, author_fname, author_lname FROM books ORDER BY 2;
+-- SELECT book_id, author_fname, author_lname, pages FROM books ORDER BY 4 DESC;
+
+-- SELECT author_fname, author_lname FROM books ORDER BY author_lname, author_fname;
