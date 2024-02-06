e6d118137bf622bb4cffe4bdce77beee3ecd7183
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-06 12:43:40 +0100

    Update 04_refining_selections.sql

--- mysql_bootcamp/04_refining_selections.sql
+++ mysql_bootcamp/04_refining_selections.sql
@@ -19,3 +19,8 @@
 -- SELECT book_id, author_fname, author_lname, pages FROM books ORDER BY 4 DESC;
 
 -- SELECT author_fname, author_lname FROM books ORDER BY author_lname, author_fname;
+-- SELECT author_lname, released_year, title FROM books ORDER BY author_lname, released_year DESC;
+
+-- SELECT CONCAT(author_fname, ' ', author_lname) AS author FROM books ORDER BY author;
+
+# LIMIT
