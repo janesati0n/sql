77438c017b19e1526832d4e3238d3a936440f2f4
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-07 10:17:20 +0100

    Update 05_aggregate_functions.sql

--- mysql_bootcamp/05_aggregate_functions.sql
+++ mysql_bootcamp/05_aggregate_functions.sql
@@ -1,6 +1,11 @@
-# Count Basics
+# COUNT
 -- SELECT COUNT(*) FROM books;
 -- SELECT COUNT(author_fname) FROM books;
 -- SELECT COUNT(DISTINCT author_fname) FROM books;
 -- SELECT COUNT(*) FROM books WHERE title LIKE '%the%';
 
+# GROUP BY
+-- SELECT author_lname, COUNT(*) AS books_written FROM books GROUP BY author_lname ORDER BY books_written DESC;
+-- SELECT released_year, COUNT(*) FROM books GROUP BY released_year;
+
+# MIN and MAX
