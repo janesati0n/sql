a4732c4d5c12f842976a62dbbd919de748fe0c75
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-07 09:56:16 +0100

    Update 05_aggregate_functions.sql

--- mysql_bootcamp/05_aggregate_functions.sql
+++ mysql_bootcamp/05_aggregate_functions.sql
@@ -1,1 +1,6 @@
 # Count Basics
+-- SELECT COUNT(*) FROM books;
+-- SELECT COUNT(author_fname) FROM books;
+-- SELECT COUNT(DISTINCT author_fname) FROM books;
+-- SELECT COUNT(*) FROM books WHERE title LIKE '%the%';
+
