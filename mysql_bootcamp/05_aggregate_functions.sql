1bee8fe953bd50951a0e9913612dd905ec31060a
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-08 10:37:38 +0100

    Update 05_aggregate_functions.sql

--- mysql_bootcamp/05_aggregate_functions.sql
+++ mysql_bootcamp/05_aggregate_functions.sql
@@ -24,3 +24,11 @@
 -- SELECT author_fname, author_lname, COUNT(*) FROM books GROUP BY author_lname, author_fname;
 -- SELECT CONCAT(author_fname, ' ', author_lname) AS author FROM books GROUP BY author;
 
+# Min/Max with Group By
+-- SELECT 
+-- 	author_fname, 
+-- 	author_lname, 
+-- 	COUNT(*) AS books_written, 
+-- 	MIN(released_year) AS earliest_release, 
+-- 	MAX(released_year) AS latest_release 
+-- FROM books GROUP BY author_lname, author_fname;
