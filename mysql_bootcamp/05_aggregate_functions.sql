36cca4097d8bf919f44ec0fc4e1ccf5017e668e0
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-08 10:06:58 +0100

    Update 05_aggregate_functions.sql

--- mysql_bootcamp/05_aggregate_functions.sql
+++ mysql_bootcamp/05_aggregate_functions.sql
@@ -14,3 +14,8 @@
 -- SELECT MIN(author_lname), MAX(author_lname) FROM books;
 
 # Subqueries
+-- SELECT title, pages FROM books ORDER BY pages DESC LIMIT 1;
+-- SELECT title, pages FROM books WHERE pages = (SELECT MAX(pages) FROM books);
+
+-- SELECT title, released_year FROM books 
+-- WHERE released_year = (SELECT MIN(released_year) FROM books);
