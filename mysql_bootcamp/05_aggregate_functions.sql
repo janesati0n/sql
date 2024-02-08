05f7045eb7cb8094f39eba8f2d1834d8bd7b5d34
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-08 10:41:03 +0100

    Update 05_aggregate_functions.sql

--- mysql_bootcamp/05_aggregate_functions.sql
+++ mysql_bootcamp/05_aggregate_functions.sql
@@ -30,5 +30,6 @@
 -- 	author_lname, 
 -- 	COUNT(*) AS books_written, 
 -- 	MIN(released_year) AS earliest_release, 
--- 	MAX(released_year) AS latest_release 
+-- 	MAX(released_year) AS latest_release, 
+--     MAX(pages) AS longest_book
 -- FROM books GROUP BY author_lname, author_fname;
