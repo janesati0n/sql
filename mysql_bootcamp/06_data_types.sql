4f5e79414ab8d267ec51e5bbc0f49b0490e9a893
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-09 13:29:49 +0100

    Update 06_data_types.sql

--- mysql_bootcamp/06_data_types.sql
+++ mysql_bootcamp/06_data_types.sql
@@ -13,9 +13,9 @@
 -- BIGINT <-2^63 ... 2^63-1>		8 bytes
 
 -- TINYINT UNSIGNED <0 ... 255>		1 byte
--- SMALLINT UNSIGNED <0 ... 65535>		2 bytes
+-- SMALLINT UNSIGNED <0 ... 65535>	2 bytes
 -- MEDIUMINT UNSIGNED <0 ... 16777215>	3 bytes
 -- INT UNSIGNED <0 ... 4294967295>	4 bytes
--- BIGINT UNSIGNED <0 ... 2^64-1>		8 bytes
+-- BIGINT UNSIGNED <0 ... 2^64-1>	8 bytes
 
 # DECIMAL
