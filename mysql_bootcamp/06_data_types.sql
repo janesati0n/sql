d766009af47faabfa44e87cae060613619634d08
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-09 13:03:54 +0100

    Update 06_data_types.sql

--- mysql_bootcamp/06_data_types.sql
+++ mysql_bootcamp/06_data_types.sql
@@ -1,1 +1,13 @@
+# CHAR vs. VARCHAR
+-- CREATE TABLE names (abbr VARCHAR(10));
+-- INSERT INTO names (abbr) VALUES ('jon'), ('juan pablo'), ('james');
 
+-- CREATE TABLE states (abbr CHAR(2));
+-- INSERT INTO states (abbr) VALUES ('CA'), ('NY');		//faster for fixed length text
+
+# INT, TINYINT, BIGINT
+-- TINYINT <-128 ... 127>			1 byte
+-- SMALLINT <-32768 ... 32767>		2 bytes
+-- MEDIUMINT <-8388608 ... 8388607>	3 bytes
+-- INT <-2147483648 ... 2147483647>	4 bytes
+-- BIGINT <-2^63 ... 2^63-1>		8 bytes
