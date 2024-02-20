96e6d5ed3e19630969ac545eda6af0ced2f014af
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-20 15:32:18 +0100

    Update 06_data_types.sql

--- mysql_bootcamp/06_data_types.sql
+++ mysql_bootcamp/06_data_types.sql
@@ -80,3 +80,10 @@
 
 -- INSERT INTO captions (text) VALUES ('just me and the girls chillin');
 -- INSERT INTO captions (text) VALUES ('beautiful sunset');
+
+-- CREATE TABLE captions2 (
+-- 	text VARCHAR(150),
+-- 	created_at TIMESTAMP default CURRENT_TIMESTAMP,
+--     updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+-- );
+
