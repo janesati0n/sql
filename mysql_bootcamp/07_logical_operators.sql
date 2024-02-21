1a68bd1dfe0ec1dd882f2fa7fe44570ba96ae647
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-21 13:55:21 +0100

    Update 07_logical_operators.sql

--- mysql_bootcamp/07_logical_operators.sql
+++ mysql_bootcamp/07_logical_operators.sql
@@ -21,5 +21,6 @@
 
 # Logical OR
 -- SELECT title, author_lname, released_year FROM books WHERE author_lname = 'Eggers' OR released_year > 2010;
+-- SELECT title, pages FROM books WHERE pages < 200 OR title LIKE '%stories';
 
 # BETWEEN
