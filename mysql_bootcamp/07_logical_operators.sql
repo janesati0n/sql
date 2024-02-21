b07e5a0e1abe3eac6215b4666e439a157ccb6298
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-21 13:53:37 +0100

    Update 07_logical_operators.sql

--- mysql_bootcamp/07_logical_operators.sql
+++ mysql_bootcamp/07_logical_operators.sql
@@ -20,4 +20,6 @@
 -- SELECT title, pages FROM books WHERE CHAR_LENGTH(title) > 30 AND pages > 500;
 
 # Logical OR
--- SELECT title, author_lname FROM books WHERE author_lname = 'Eggers' OR released_year > 2010;
+-- SELECT title, author_lname, released_year FROM books WHERE author_lname = 'Eggers' OR released_year > 2010;
+
+# BETWEEN
