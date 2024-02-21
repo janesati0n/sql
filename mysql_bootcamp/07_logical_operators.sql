23eceabcf28125d15eb4b29eff511762cb8788f0
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-21 16:02:43 +0100

    Update 07_logical_operators.sql

--- mysql_bootcamp/07_logical_operators.sql
+++ mysql_bootcamp/07_logical_operators.sql
@@ -89,3 +89,28 @@
 # IS NULL
 -- SELECT * FROM books WHERE author_lname IS NULL;
 -- SELECT * FROM books WHERE author_lname IS NOT NULL;
+
+# Excercise
+-- SELECT * FROM books WHERE released_year < 1980;
+-- SELECT * FROM books WHERE author_lname IN ('Eggers', 'Chabon');
+-- SELECT * FROM books WHERE author_lname = 'Lahiri' AND released_year > 2000;
+-- SELECT * FROM books WHERE pages BETWEEN 100 AND 200;
+-- SELECT title, author_lname FROM books WHERE author_lname LIKE 'C%' OR author_lname LIKE 'S%';
+-- SELECT title, author_lname FROM books WHERE SUBSTR(author_lname, 1, 1) IN ('C', 'S');
+
+-- SELECT title, author_lname,
+-- 	CASE
+-- 		WHEN title LIKE '%stories%' THEN 'Short Stories'
+-- 		WHEN title = 'Just Kids' THEN 'Memoir'
+-- 		WHEN title LIKE 'A Heartbreaking Work %' THEN 'Memoir'
+-- 		ELSE 'Novel'
+-- 	END AS type
+-- FROM books;
+
+-- SELECT author_fname, author_lname, 
+-- 	CASE
+-- 		WHEN COUNT(*) = 1 THEN '1 book'
+-- 		ELSE CONCAT(COUNT(*), ' books')
+-- 	END AS count
+-- FROM books 
+-- 	GROUP BY author_fname, author_lname;
