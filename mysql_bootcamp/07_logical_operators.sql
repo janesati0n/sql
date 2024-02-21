5ec73ab242d15e3eafd591e72dc7f7ab9f1cb941
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-21 13:06:18 +0100

    Update 07_logical_operators.sql

--- mysql_bootcamp/07_logical_operators.sql
+++ mysql_bootcamp/07_logical_operators.sql
@@ -17,3 +17,4 @@
 -- SELECT * FROM books WHERE author_lname = 'Eggers' AND released_year > 2010;
 -- SELECT * FROM books WHERE author_lname = 'Eggers' && released_year > 2010;
 -- SELECT * FROM books WHERE author_lname = 'Eggers' AND released_year > 2010 AND title LIKE '%novel%';
+-- SELECT title, pages FROM books WHERE CHAR_LENGTH(title) > 30 AND pages > 500;
