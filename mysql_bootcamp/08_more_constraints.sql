74be75ab4326490f5c3c3cbd42bf21537324964a
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-22 09:37:04 +0100

    Update 08_more_constraints.sql

--- mysql_bootcamp/08_more_constraints.sql
+++ mysql_bootcamp/08_more_constraints.sql
@@ -1,1 +1,18 @@
 # UNIQUE
+-- CREATE TABLE companies (
+-- 	supplier_id INT AUTO_INCREMENT,
+-- 	name VARCHAR(255) NOT NULL,
+-- 	phone VARCHAR(15) NOT NULL UNIQUE,
+-- 	address VARCHAR(255) NOT NULL,
+-- 	PRIMARY KEY (supplier_id)
+-- );
+
+-- CREATE TABLE contacts (
+-- 	name VARCHAR(100) NOT NULL,
+-- 	phone VARCHAR(15) NOT NULL UNIQUE
+-- );
+
+-- INSERT INTO contacts (name, phone) VALUES ('billybob', '8781213455');
+-- INSERT INTO contacts (name, phone) VALUES ('timmytimtim', '8781213455');	//Error
+
+# CHECK
