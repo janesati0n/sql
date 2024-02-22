1a82f2d31a816a781eaffa0db503af5be6af5070
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-22 09:46:29 +0100

    Update 08_more_constraints.sql

--- mysql_bootcamp/08_more_constraints.sql
+++ mysql_bootcamp/08_more_constraints.sql
@@ -16,3 +16,24 @@
 -- INSERT INTO contacts (name, phone) VALUES ('timmytimtim', '8781213455');	//Error
 
 # CHECK
+-- CREATE TABLE partiers (
+-- 	name VARCHAR(50),
+-- 	age INT CHECK (age > 18)
+-- );
+
+-- CREATE TABLE users (
+-- 	username VARCHAR(20) NOT NULL,
+-- 	age INT CHECK (age > 0)
+-- );
+
+-- INSERT INTO users (username, age) VALUES ('bluethecat', 50);
+-- INSERT INTO users (username, age) VALUES ('coltsteele', -3);	//Error
+
+-- CREATE TABLE palindromes (
+-- 	word VARCHAR(100) CHECK(REVERSE(word) = word)
+-- );
+
+-- INSERT INTO palindromes (word) VALUES ('racecar');
+-- INSERT INTO palindromes (word) VALUES ('mommy'); 	//Error
+
+# Named Constraints
