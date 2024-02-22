0e5db2e6fba058de56145cc9c96d41a191bf6433
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-22 09:56:45 +0100

    Update 08_constraints.sql

--- mysql_bootcamp/08_constraints.sql
+++ mysql_bootcamp/08_constraints.sql
@@ -37,3 +37,24 @@
 -- INSERT INTO palindromes (word) VALUES ('mommy'); 	//Error
 
 # Named Constraints
+-- CREATE TABLE partiers2 (
+-- 	name VARCHAR(50),
+-- 	age INT,
+-- 	CONSTRAINT age_over_18 CHECK (age > 18)
+-- );
+
+-- CREATE TABLE users2 (
+-- 	username VARCHAR(20) NOT NULL,
+-- 	age INT,
+-- 	CONSTRAINT age_not_negative CHECK (age > 0)
+-- );
+
+-- INSERT INTO user2 (username, age) VALUES ('chickenlady', -9);	//Error
+
+-- CREATE TABLE palindromes2 (
+-- 	word VARCHAR(100),
+-- 	CONSTRAINT word_is_palindrome CHECK(REVERSE(word) = word)
+-- );
+
+-- INSERT INTO palindromes2 VALUES ('mom');
+-- INSERT INTO palindromes2 VALUES ('momma');	//Error
