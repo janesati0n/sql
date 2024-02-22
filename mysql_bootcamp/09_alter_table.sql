45c0fef19984b885c67dabeab8442f61a640be22
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-22 13:28:58 +0100

    Update 09_alter_table.sql

--- mysql_bootcamp/09_alter_table.sql
+++ mysql_bootcamp/09_alter_table.sql
@@ -1,2 +1,6 @@
 # Adding Columns
 -- ALTER TABLE companies ADD COLUMN city VARCHAR(25);
+-- ALTER TABLE companies ADD COLUMN phone VARCHAR(15);
+-- ALTER TABLE companies ADD COLUMN employee_count INT NOT NULL DEFAULT 1;
+
+# Dropping Columns
