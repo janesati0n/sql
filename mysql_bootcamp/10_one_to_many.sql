209053aae993e7115d719d656b85fb636ea32333
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-23 15:35:19 +0100

    Update 10_one_to_many.sql

--- mysql_bootcamp/10_one_to_many.sql
+++ mysql_bootcamp/10_one_to_many.sql
@@ -35,3 +35,13 @@
 -- SELECT * FROM customers, orders;		//Not really helpful
 
 # Inner Joins
+-- SELECT * FROM customers 
+-- JOIN orders ON customers.id = orders.customer_id;
+
+-- SELECT first_name, last_name, order_date, amount FROM customers 
+-- JOIN orders ON customers.id = orders.customer_id;
+
+-- SELECT * FROM orders 
+-- JOIN customers ON customers.id = orders.customer_id;
+
+# Inner Joins with Group By
