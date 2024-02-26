04ba8082c22e13cbcc74d2dd849715629f1f0070
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-26 10:26:16 +0100

    Update 10_one_to_many.sql

--- mysql_bootcamp/10_one_to_many.sql
+++ mysql_bootcamp/10_one_to_many.sql
@@ -51,3 +51,9 @@
 -- 	ORDER BY total DESC;
 
 # Left Join
+-- SELECT first_name, last_name, order_date, amount FROM customers
+-- LEFT JOIN orders ON orders.customer_id = customers.id;
+
+-- SELECT order_date, amount, first_name, last_name FROM orders
+-- LEFT JOIN customers ON customers.id = orders.customer_id;
+
