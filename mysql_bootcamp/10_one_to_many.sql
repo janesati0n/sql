f2992e98cd961045ad2d9db891b05f2b5162c052
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-26 11:03:36 +0100

    Update 10_one_to_many.sql

--- mysql_bootcamp/10_one_to_many.sql
+++ mysql_bootcamp/10_one_to_many.sql
@@ -66,3 +66,26 @@
 -- INSERT INTO orders (amount, order_date) VALUES (100, CURDATE());
 -- SELECT first_name, last_name, order_date, amount FROM customers
 -- RIGHT JOIN orders ON customers.id = orders.customer_id;
+
+# Excercise
+CREATE TABLE students (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	first_name VARCHAR (12)
+);
+
+CREATE TABLE papers (
+	title VARCHAR (50),
+	grade INT,
+	student_id INT,
+    FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE
+);
+
+INSERT INTO students (first_name) VALUES
+('Caleb'), ('Samantha'), ('Raj'), ('Carlos'), ('Lisa');
+
+INSERT INTO papers (student_id, title, grade) VALUES
+(1, 'My First Book Report', 60),
+(1, 'My Second Book Report', 75),
+(2, 'Russian Lit Through The Ages', 94),
+(2, 'De Montaigne and The Art of The Essay', 98),
+(4, 'Borges and Magical Realism', 89);
