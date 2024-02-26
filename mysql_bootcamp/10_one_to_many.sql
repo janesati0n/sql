2f393a8e68d1d7a2fef8a79885d928272b819f28
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-26 10:30:18 +0100

    Update 10_one_to_many.sql

--- mysql_bootcamp/10_one_to_many.sql
+++ mysql_bootcamp/10_one_to_many.sql
@@ -57,3 +57,7 @@
 -- SELECT order_date, amount, first_name, last_name FROM orders
 -- LEFT JOIN customers ON customers.id = orders.customer_id;
 
+# Left Join with Group By
+-- SELECT first_name, last_name, IFNULL(SUM(amount), 0) AS money_spent FROM customers
+-- LEFT JOIN orders ON customers.id = orders.customer_id
+-- GROUP BY first_name, last_name;
