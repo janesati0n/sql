2a20bffe4e191716c4a40c258e4f0f4653d24437
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-27 09:14:16 +0100

    Update 10_one_to_many.sql

--- mysql_bootcamp/10_one_to_many.sql
+++ mysql_bootcamp/10_one_to_many.sql
@@ -67,25 +67,65 @@
 -- SELECT first_name, last_name, order_date, amount FROM customers
 -- RIGHT JOIN orders ON customers.id = orders.customer_id;
 
+# On Delete Cascade
+-- DELETE FROM customers WHERE last_name = 'George'; //can't
+
+-- CREATE TABLE orders (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	order_date DATE,
+-- 	amount DECIMAL (8,2),
+-- 	customer_id INT,
+-- 	FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE CASCADE
+-- );
+
+-- DELETE FROM customers WHERE last_name = 'George';	//now we can
+
 # Excercise
-CREATE TABLE students (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	first_name VARCHAR (12)
-);
+-- CREATE TABLE students (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	first_name VARCHAR (12)
+-- );
 
-CREATE TABLE papers (
-	title VARCHAR (50),
-	grade INT,
-	student_id INT,
-    FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE
-);
+-- CREATE TABLE papers (
+-- 	title VARCHAR (50),
+-- 	grade INT,
+-- 	student_id INT,
+--     FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE
+-- );
 
-INSERT INTO students (first_name) VALUES
-('Caleb'), ('Samantha'), ('Raj'), ('Carlos'), ('Lisa');
+-- INSERT INTO students (first_name) VALUES
+-- ('Caleb'), ('Samantha'), ('Raj'), ('Carlos'), ('Lisa');
 
-INSERT INTO papers (student_id, title, grade) VALUES
-(1, 'My First Book Report', 60),
-(1, 'My Second Book Report', 75),
-(2, 'Russian Lit Through The Ages', 94),
-(2, 'De Montaigne and The Art of The Essay', 98),
-(4, 'Borges and Magical Realism', 89);
+-- INSERT INTO papers (student_id, title, grade) VALUES
+-- (1, 'My First Book Report', 60),
+-- (1, 'My Second Book Report', 75),
+-- (2, 'Russian Lit Through The Ages', 94),
+-- (2, 'De Montaigne and The Art of The Essay', 98),
+-- (4, 'Borges and Magical Realism', 89);
+
+-- SELECT first_name, title, grade FROM students
+-- JOIN papers ON students.id = papers.student_id
+-- ORDER BY grade DESC;
+
+-- SELECT first_name, title, grade FROM students
+-- LEFT JOIN papers ON students.id = papers.student_id;
+
+-- SELECT first_name, IFNULL(title, 'MISSING'), IFNULL(grade, 0) FROM students
+-- LEFT JOIN papers ON students.id = papers.student_id;
+
+-- SELECT first_name, IFNULL(AVG(grade), 0) AS average FROM students
+-- LEFT JOIN papers ON students.id = papers.student_id
+-- GROUP BY first_name
+-- ORDER BY average DESC;
+
+-- SELECT 
+-- 	first_name, 
+-- 	IFNULL(AVG(grade), 0) AS average, 
+-- 	CASE
+-- 		WHEN IFNULL(AVG(grade), 0) >= 75 THEN 'PASSING'
+-- 		ELSE 'FAILING'
+--     END AS passing_status
+-- 	FROM students
+-- 	LEFT JOIN papers ON students.id = papers.student_id
+-- 	GROUP BY first_name
+-- 	ORDER BY average DESC;
