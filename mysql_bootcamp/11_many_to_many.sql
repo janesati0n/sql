47063067a0637886b4c4edc2682262681b1a4bb8
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-27 10:17:08 +0100

    Update 11_many_to_many.sql

--- mysql_bootcamp/11_many_to_many.sql
+++ mysql_bootcamp/11_many_to_many.sql
@@ -1,1 +1,25 @@
-# Basics
+# Creating Tables
+-- CREATE DATABASE tv_db;
+
+-- CREATE TABLE reviewers (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	first_name VARCHAR(50) NOT NULL,
+--     last_name VARCHAR(50) NOT NULL
+-- );
+
+-- CREATE TABLE series (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+--     title VARCHAR(100),
+--     released_year YEAR,
+--     genre VARCHAR(100)
+-- );
+
+-- CREATE TABLE reviews (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+--     rating DECIMAL(2,1),
+--     series_id INT,
+--     reviewer_id INT,
+--     FOREIGN KEY (series_id) REFERENCES series(id),
+--     FOREIGN KEY (reviewer_id) REFERENCES reviewers(id)
+-- );
+
