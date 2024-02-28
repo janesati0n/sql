6369718b389a10b59b5209d59cc3bea16f7ff8e4
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-28 15:34:21 +0100

    Update 11_many_to_many.sql

--- mysql_bootcamp/11_many_to_many.sql
+++ mysql_bootcamp/11_many_to_many.sql
@@ -32,3 +32,29 @@
 -- GROUP BY genre;
 
 # TV Series Challenge 6
+-- SELECT 
+-- 	first_name, 
+--     last_name, 
+-- 	COUNT(rating) AS COUNT, 
+-- 	IFNULL(MIN(rating), 0) AS MIN, 
+-- 	IFNULL(MAX(rating), 0) AS MAX, 
+-- 	ROUND(IFNULL(AVG(rating), 0), 2) AS AVG, 
+-- 	CASE
+-- 		WHEN COUNT(rating) > 0 THEN 'ACTIVE'
+-- 		ELSE 'INACTIVE'
+--     END AS STATUS
+-- FROM reviewers 
+-- LEFT JOIN reviews ON reviewers.id = reviews.reviewer_id
+-- GROUP BY first_name, last_name;
+
+-- SELECT 
+-- 	first_name, 
+--     last_name, 
+-- 	COUNT(rating) AS COUNT, 
+-- 	IFNULL(MIN(rating), 0) AS MIN, 
+-- 	IFNULL(MAX(rating), 0) AS MAX, 
+-- 	ROUND(IFNULL(AVG(rating), 0), 2) AS AVG, 
+-- 	IF(COUNT(rating) > 0, 'ACTIVE', 'INACTIVE') AS STATUS
+-- FROM reviewers 
+-- LEFT JOIN reviews ON reviewers.id = reviews.reviewer_id
+-- GROUP BY first_name, last_name;
