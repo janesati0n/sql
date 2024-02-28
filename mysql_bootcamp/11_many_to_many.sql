dcfd145b60806d9beea3877b5c68f6b0ff1bdd52
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-28 12:03:40 +0100

    Update 11_many_to_many.sql

--- mysql_bootcamp/11_many_to_many.sql
+++ mysql_bootcamp/11_many_to_many.sql
@@ -2,9 +2,9 @@
 -- SELECT title, rating FROM reviews
 -- JOIN series ON reviews.series_id = series.id;
 
+# TV Series Challenge 2
 -- SELECT title, AVG(rating) AS average FROM reviews
 -- JOIN series ON reviews.series_id = series.id
 -- GROUP BY title
--- ORDER BY average DESC;
+-- ORDER BY average;
 
-# TV Series Challenge 2
