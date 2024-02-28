d9720b060d13952144de7f2e55275f3688d67367
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-02-28 14:58:35 +0100

    Update 11_many_to_many.sql

--- mysql_bootcamp/11_many_to_many.sql
+++ mysql_bootcamp/11_many_to_many.sql
@@ -14,3 +14,7 @@
 -- ORDER BY avg_rating;
 
 # TV Series Challenge 3
+-- SELECT first_name, last_name, rating FROM reviewers
+-- JOIN reviews ON reviewers.id = reviews.reviewer_id;
+
+# TV Series Challenge 4
