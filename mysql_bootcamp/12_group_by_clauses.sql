a44d1323e9fd51350e0ceec3a3953c85234804ea
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-03-04 14:46:44 +0100

    Update 12_group_by_clauses.sql

--- mysql_bootcamp/12_group_by_clauses.sql
+++ mysql_bootcamp/12_group_by_clauses.sql
@@ -1,1 +1,9 @@
 # HAVING clause
+-- SELECT 
+-- 	title, 
+-- 	AVG(rating) AS average,
+--     COUNT(rating) AS review_count
+-- FROM full_reviews 
+-- GROUP BY title HAVING COUNT(rating) > 1;
+
+# WITH ROLLUP
