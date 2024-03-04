dfbd4fe41fed140e46df59e6629be0aa5ae3a270
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-03-04 14:38:18 +0100

    Update 12_views.sql

--- mysql_bootcamp/12_views.sql
+++ mysql_bootcamp/12_views.sql
@@ -17,3 +17,10 @@
 -- DELETE FROM ordered_series WHERE title = 'The Great';
 
 # Replacing/Altering Views
+-- CREATE OR REPLACE VIEW ordered_series AS
+-- SELECT * FROM series ORDER BY released_year DESC;
+
+-- ALTER VIEW ordered_series AS 
+-- SELECT * FROM series ORDER BY released_year;
+
+-- DROP VIEW ordered_series;
