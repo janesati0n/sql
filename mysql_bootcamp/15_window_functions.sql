f5a3437358f641ede0b180f753ac975bcc1e53dc
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-03-06 15:46:12 +0100

    Update 15_window_functions.sql

--- mysql_bootcamp/15_window_functions.sql
+++ mysql_bootcamp/15_window_functions.sql
@@ -100,3 +100,12 @@
 -- FROM employees ORDER BY department;
 
 # NTILE()
+-- SELECT 
+-- 	emp_no, 
+-- 	department, 
+-- 	salary,
+-- 	NTILE(4) OVER(PARTITION BY department ORDER BY salary DESC) AS dept_salary_quartile,
+-- 	NTILE(4) OVER(ORDER BY salary DESC) AS salary_quartile
+-- FROM employees;
+
+# FIRST_VALUE
