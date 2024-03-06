31895750dfaecb0124e43063fcc070bb72610d58
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-03-06 16:03:37 +0100

    Update 15_window_functions.sql

--- mysql_bootcamp/15_window_functions.sql
+++ mysql_bootcamp/15_window_functions.sql
@@ -109,3 +109,10 @@
 -- FROM employees;
 
 # FIRST_VALUE
+-- SELECT 
+-- 	emp_no, 
+-- 	department, 
+-- 	salary,
+-- 	FIRST_VALUE(emp_no) OVER(PARTITION BY department ORDER BY salary DESC) as highest_paid_dept,
+-- 	FIRST_VALUE(emp_no) OVER(ORDER BY salary DESC) as highest_paid_overall
+-- FROM employees;
