38b749ac52958a1ebc0d1d96caa624930973f7fb
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-03-06 16:09:33 +0100

    Update 15_window_functions.sql

--- mysql_bootcamp/15_window_functions.sql
+++ mysql_bootcamp/15_window_functions.sql
@@ -116,3 +116,25 @@
 -- 	FIRST_VALUE(emp_no) OVER(PARTITION BY department ORDER BY salary DESC) as highest_paid_dept,
 -- 	FIRST_VALUE(emp_no) OVER(ORDER BY salary DESC) as highest_paid_overall
 -- FROM employees;
+
+# LEAD and LAG
+-- SELECT 
+-- 	emp_no, 
+-- 	department, 
+-- 	salary,
+-- 	salary - LAG(salary) OVER(ORDER BY salary DESC) as salary_diff
+-- FROM employees;
+
+-- SELECT 
+-- 	emp_no, 
+-- 	department, 
+-- 	salary,
+-- 	salary - LEAD(salary) OVER(ORDER BY salary DESC) as salary_diff
+-- FROM employees;
+
+-- SELECT 
+-- 	emp_no, 
+-- 	department, 
+-- 	salary,
+-- 	salary - LAG(salary) OVER(PARTITION BY department ORDER BY salary DESC) as dept_salary_diff
+-- FROM employees;
