658e02b6fc247b10c3763d075d0ec0637cad6367
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-03-08 13:16:30 +0100

    Update schemas.sql

--- instagram_project/schemas.sql
+++ instagram_project/schemas.sql
@@ -5,3 +5,25 @@ CREATE TABLE users (
 	username VARCHAR(255) UNIQUE NOT NULL,
 	created_at TIMESTAMP DEFAULT NOW()
 );
+
+CREATE TABLE photos (
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	image_url ,
+	user_id ,
+	created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE comments (
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	commented_text ,
+	user_id ,
+	photo_id ,
+	created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE likes (
+	user_id ,
+	photo_id ,
+	created_at TIMESTAMP DEFAULT NOW()
+);
+
