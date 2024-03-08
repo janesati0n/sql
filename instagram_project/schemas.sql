d41da71a87da49c7a133884319145d9cef7d201e
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-03-08 14:18:04 +0100

    Update schemas.sql

--- instagram_project/schemas.sql
+++ instagram_project/schemas.sql
@@ -34,11 +34,25 @@
 -- 	PRIMARY KEY(user_id, photo_id)
 -- );
 
-CREATE TABLE follows (
-	follower_id INT NOT NULL,
-    followee_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
-    FOREIGN KEY (follower_id) REFERENCES users(id),
-    FOREIGN KEY (followee_id) REFERENCES users(id),
-    PRIMARY KEY(follower_id, followee_id)
-);
+-- CREATE TABLE follows (
+-- 	follower_id INT NOT NULL,
+--     followee_id INT NOT NULL,
+--     created_at TIMESTAMP DEFAULT NOW(),
+--     FOREIGN KEY (follower_id) REFERENCES users(id),
+--     FOREIGN KEY (followee_id) REFERENCES users(id),
+--     PRIMARY KEY(follower_id, followee_id)
+-- );
+
+-- CREATE TABLE tags (
+-- 	id INT AUTO_INCREMENT PRIMARY KEY,
+--     tag_name VARCHAR(255) UNIQUE,
+--     created_at TIMESTAMP DEFAULT NOW()
+-- );
+
+-- CREATE TABLE photo_tags (
+-- 	photo_id INT NOT NULL,
+--     tag_id INT NOT NULL,
+--     FOREIGN KEY (photo_id) REFERENCES photos(id),
+--     FOREIGN KEY (tag_id) REFERENCES tags(id),
+--     PRIMARY KEY (photo_id, tag_id)
+-- );
