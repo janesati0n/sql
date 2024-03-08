ca459f33ddbcf45de710bb1897e3202fbcc6a177
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-03-08 14:47:00 +0100

    Update 03_challenges.sql

--- instagram_project/03_challenges.sql
+++ instagram_project/03_challenges.sql
@@ -1,4 +1,15 @@
 # IG Challenge 1
+# Find 5 oldest users
 -- SELECT * FROM users ORDER BY created_at LIMIT 5;
 
 # IG Challenge 2
+# What day of the week do most users register on
+-- SELECT 
+-- 	DAYNAME(created_at) AS day,
+--     COUNT(*) AS total_users
+-- FROM users
+-- GROUP BY day
+-- ORDER BY total_users DESC;
+
+# IG Challenge 3
+#
