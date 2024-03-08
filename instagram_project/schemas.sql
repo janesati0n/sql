9d05efbd4693a60a91d391e9ccbee1ee3beb605b
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-03-08 13:21:32 +0100

    Update schemas.sql

--- instagram_project/schemas.sql
+++ instagram_project/schemas.sql
@@ -1,29 +1,35 @@
-CREATE DATABASE ig_project;
+-- CREATE DATABASE ig_project;
+-- USE ig_project;
 
 CREATE TABLE users (
-	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	username VARCHAR(255) UNIQUE NOT NULL,
 	created_at TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE photos (
-	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	image_url ,
 	user_id ,
-	created_at TIMESTAMP DEFAULT NOW()
+	created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE comments (
-	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	commented_text ,
 	user_id ,
 	photo_id ,
-	created_at TIMESTAMP DEFAULT NOW()
+	created_at TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (photo_id) REFERENCES photos(id)
 );
 
 CREATE TABLE likes (
 	user_id ,
 	photo_id ,
-	created_at TIMESTAMP DEFAULT NOW()
+	created_at TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (photo_id) REFERENCES photos(id)
 );
 
