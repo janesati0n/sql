f8c51a769c4495827fe8f442c1a9a8bce7cc90ff
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-03-08 13:40:37 +0100

    Update schemas.sql

--- instagram_project/schemas.sql
+++ instagram_project/schemas.sql
@@ -1,29 +1,29 @@
 -- CREATE DATABASE ig_project;
 -- USE ig_project;
 
-CREATE TABLE users (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	username VARCHAR(255) UNIQUE NOT NULL,
-	created_at TIMESTAMP DEFAULT NOW()
-);
+-- CREATE TABLE users (
+-- 	id INT AUTO_INCREMENT PRIMARY KEY,
+-- 	username VARCHAR(255) UNIQUE NOT NULL,
+-- 	created_at TIMESTAMP DEFAULT NOW()
+-- );
 
-CREATE TABLE photos (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	image_url ,
-	user_id ,
-	created_at TIMESTAMP DEFAULT NOW(),
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
+-- CREATE TABLE photos (
+-- 	id INT AUTO_INCREMENT PRIMARY KEY,
+-- 	image_url VARCHAR(255) NOT NULL,
+-- 	user_id INT NOT NULL,
+-- 	created_at TIMESTAMP DEFAULT NOW(),
+--     FOREIGN KEY (user_id) REFERENCES users(id)
+-- );
 
-CREATE TABLE comments (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	commented_text ,
-	user_id ,
-	photo_id ,
-	created_at TIMESTAMP DEFAULT NOW(),
-	FOREIGN KEY (user_id) REFERENCES users(id),
-	FOREIGN KEY (photo_id) REFERENCES photos(id)
-);
+-- CREATE TABLE comments (
+-- 	id INT AUTO_INCREMENT PRIMARY KEY,
+-- 	commented_text VARCHAR(255) NOT NULL,
+-- 	user_id INT NOT NULL,
+-- 	photo_id INT NOT NULL,
+-- 	created_at TIMESTAMP DEFAULT NOW(),
+-- 	FOREIGN KEY (user_id) REFERENCES users(id),
+-- 	FOREIGN KEY (photo_id) REFERENCES photos(id)
+-- );
 
 CREATE TABLE likes (
 	user_id ,
